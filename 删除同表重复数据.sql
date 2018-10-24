d21fd0de9ee2309c1421ae658ba1990a28fe1469
ckx0709 <178709230@qq.com>
2018-10-24 18:16:03 +0800

    相似度函数

--- 删除同表重复数据.sql
+++ 删除同表重复数据.sql
@@ -1,6 +1,8 @@
 --删除表中完全重复的两条数据 
 delete from ckx001 where id = (select id from ckx001 group by id having count(1) > 1)
-and rowid != (select min(rowid) from ckx001 group by id having count(1) > 1)
+and rowid != (select min(rowid) from ckx001 group by id having count(1) > 1);
+--两字符串相似度函数
+select sys.utl_match.edit_distance_similarity('为中华之崛起而读书','为中华之 崛起而读书') lim  from dual;
 /*
 分析函数
 */
