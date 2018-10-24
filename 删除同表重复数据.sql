7c1dd9ece90d50a89f65a6cf388c0c02d60a1c8c
ckx0709 <178709230@qq.com>
2018-10-24 15:48:41 +0800

    添加分析函数

--- 删除同表重复数据.sql
+++ 删除同表重复数据.sql
@@ -1,3 +1,9 @@
 --删除表中完全重复的两条数据 
 delete from ckx001 where id = (select id from ckx001 group by id having count(1) > 1)
 and rowid != (select min(rowid) from ckx001 group by id having count(1) > 1)
+/*
+分析函数
+*/
+--排名
+select t.*,dense_rank() over(order by t.sarl ) from ckx001 t;
+select t.*,rank() over(order by t.sarl ) from ckx001 t;
